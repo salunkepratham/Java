4daa5ea9e6401f331d3ee98298846c914f0e9970
salunkepratham <50434989+salunkepratham@users.noreply.github.com>
2019-05-21 11:46:46 +0530

    Add files via upload

---
+++ java_program/String.java
@@ -0,0 +1,12 @@
+class Teststring{
+    public static void main(String[] args) {
+        
+        String s= "Hello";
+       // String s1= "Welcome";
+       // String s2= "Hello";
+      //  String s3= "hello";
+       // System.out.println("s1.s2="+s1);
+        System.out.println("s.s2="+s);
+        System.out.println("s.s3="+s);
+    }
+}
